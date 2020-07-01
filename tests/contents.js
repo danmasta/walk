cd20d798ba626bec1cea451f1607999af29a6188
Daniel Smith <dannmasta@gmail.com>
2020-07-01 13:15:24 -0700

    update tests

--- tests/contents.js
+++ tests/contents.js
@@ -2,7 +2,7 @@ describe('contents', () => {
 
     it('should get contents async', () => {
 
-        return walk.contents('./tests').then(res => {
+        return walk('./tests').contents().promise().then(res => {
             expect(res).to.exist;
             expect(res).to.be.an('array');
             expect(res.length).to.be.above(1);
@@ -13,7 +13,7 @@ describe('contents', () => {
 
     it('should get contents sync', () => {
 
-        let res = walk.contents.sync('./tests');
+        let res = walk('./tests').contents().sync();
 
         expect(res).to.exist;
         expect(res).to.be.an('array');
@@ -22,11 +22,43 @@ describe('contents', () => {
 
     });
 
-    it('should require files instead of reading them', () => {
+    it('should get file contents by requiring them', () => {
 
-        let res = walk.contents.sync('./index', { require: true });
+        let res = walk('./index').require().sync();
 
         expect(res[0].contents).to.be.a('function');
+
+    });
+
+    it('should get file contents as buffer', () => {
+
+        let res = walk('./index').buffer().sync();
+
+        expect(res[0].contents).to.be.instanceOf(Buffer);
+
+    });
+
+    it('should get file contents as string', () => {
+
+        let res = walk('./index').contents().sync();
+
+        expect(res[0].contents).to.be.a('string');
+
+    });
+
+    it('should get file contents as stream', () => {
+
+        let res = walk('./index').stream().sync();
+
+        expect(res[0].contents).to.be.instanceOf(stream.Readable);
+
+    });
+
+    it('should disable reading of file contents', () => {
+
+        let res = walk('./index', { read: false }).sync();
+
+        expect(res[0].contents).to.equal(null);
 
     });
 
