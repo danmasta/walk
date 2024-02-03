a4dfe434ca5e7720dbc8935ec315ab9e60a5af50
Daniel Smith <dannmasta@gmail.com>
2024-02-03 08:45:38 -0800

    update tests

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const chai = require('chai');
+const stream = require('stream');
 const walk = require('../index');
 const File = require('../lib/file');
 const util = require('../lib/util');
-const stream = require('stream');
+const sync = require('../sync');
 
 beforeEach(() => {
-    global.path = path;
-    global.assert = chai.assert;
-    global.expect = chai.expect;
-    global.should = chai.should();
-    global.walk = walk;
-    global.File = File;
-    global.util = util;
-    global.stream = stream;
-    global.testsFileCount = walk('./tests').sync().length;
+    return import('chai').then(chai => {
+        global.assert = chai.assert;
+        global.expect = chai.expect;
+        global.should = chai.should();
+        global.stream = stream;
+        global.walk = walk;
+        global.File = File;
+        global.util = util;
+        global.sync = sync;
+    });
 });
--- tests/file.js
+++ tests/file.js
@@ -1,9 +1,14 @@
-describe('file', () => {
+describe('File', () => {
 
-    it('should have correct properties', () => {
+    it('should be an instance of File', () => {
+        return walk('./index').map(file => {
+            expect(file).to.be.instanceOf(File);
+        });
+    });
+
+    it('should have correct properties and methods', () => {
 
         return walk('./index').map(file => {
-
             expect(file.cwd).to.exist;
             expect(file.root).to.exist;
             expect(file.path).to.exist;
@@ -14,16 +19,18 @@ describe('file', () => {
             expect(file.name).to.exist;
             expect(file.ext).to.exist;
             expect(file.stat).to.exist;
-            expect(file.contents).to.equal(null);
-
-        });
-
-    });
-
-    it('should have correct methods', () => {
-
-        return walk('./index').map(file => {
-
+            expect(file.contents).to.be.undefined;
+            expect(file.encoding).to.exist;
+            expect(file.createReadStream).to.be.a('function');
+            expect(file.createWriteStream).to.be.a('function');
+            expect(file.append).to.be.a('function');
+            expect(file.read).to.be.a('function');
+            expect(file.readAsString).to.be.a('function');
+            expect(file.readAsBuffer).to.be.a('function');
+            expect(file.readStr).to.be.a('function');
+            expect(file.readBuf).to.be.a('function');
+            expect(file.write).to.be.a('function');
+            expect(file.require).to.be.a('function');
             expect(file.isBuffer).to.be.a('function');
             expect(file.isStream).to.be.a('function');
             expect(file.isNull).to.be.a('function');
@@ -36,17 +43,60 @@ describe('file', () => {
             expect(file.isFile).to.be.a('function');
             expect(file.isSocket).to.be.a('function');
             expect(file.isEmpty).to.be.a('function');
-
+            expect(file.getEncodingFromBOM).to.be.a('function');
         });
 
     });
 
-    it('should have contents', () => {
+    it('should read contents as string async', () => {
 
-        return walk('./index').contents().map(file => {
+        return walk('./index').map(file => {
+            return file.readStr().then(res => {
+                expect(res).to.be.a('string');
+            });
+        });
 
-            expect(file.contents).to.exist;
+    });
 
+    it('should read contents as string sync', () => {
+
+        sync('./index').map(file => {
+            expect(file.readStr()).to.be.a('string');
+        });
+
+    });
+
+    it('should read contents by require', () => {
+
+        return walk('./index').map(file => {
+            expect(file.require()).to.be.a('function');
+        });
+
+    });
+
+    it('should read contents as buffer', () => {
+
+        return walk('./index').map(file => {
+            return file.readBuf().then(res => {
+                expect(res).to.be.instanceOf(Buffer);
+            });
+        });
+
+    });
+
+    it('should read contents as read stream', () => {
+
+        return walk('./index').map(file => {
+            expect(file.createReadStream()).to.be.instanceOf(stream.Readable);
+        });
+
+    });
+
+    it('should set relative path based on root', () => {
+
+        return walk('./index').map(file => {
+            expect(file.relative).to.equal('');
+            expect(file.relativeFromCwd).to.equal('index.js');
         });
 
     });
