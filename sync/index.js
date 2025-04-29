73385ea3a995b26c79e8d7a5367cfeed0f176dc2
Daniel Smith <dannmasta@gmail.com>
2025-04-29 12:44:01 -0700

    remove sync variants

--- sync/index.js
+++
@@ -1,6 +0,0 @@
-const Walk = require('./walk');
-const walk = Walk.factory();
-
-exports = module.exports = walk;
-exports.walk = walk;
-exports.Walk = Walk;
--- sync/index.mjs
+++
@@ -1,6 +0,0 @@
-import Walk from './walk.js';
-const walk = Walk.factory();
-
-export {
-    walk as default, walk, Walk
-};
