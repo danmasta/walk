b7d455eac92b4089240a21cc548625892517601b
Daniel Smith <dannmasta@gmail.com>
2025-04-29 12:44:29 -0700

    update main entrypoint to esm, update exports

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const Walk = require('./lib/walk');
-const walk = Walk.factory();
+import { BREAK } from 'lo';
+import { File } from './lib/file.js';
+import { FileResolver } from './lib/resolver.js';
+import { FileError, NotFoundError, NotResolvedError, WalkError } from './lib/util.js';
+import { Walk } from './lib/walk.js';
 
-exports = module.exports = walk;
-exports.walk = walk;
-exports.Walk = Walk;
+const walk = Walk.factory();
+const sync = Walk.factory(true);
+
+export {
+    BREAK,
+    File,
+    FileError,
+    FileResolver,
+    NotFoundError,
+    NotResolvedError,
+    sync,
+    Walk,
+    walk,
+    WalkError
+};
+
+export default walk;
